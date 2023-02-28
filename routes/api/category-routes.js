a671658e3fd014e7e36decd48dc10d5e6f19599f
Mia Ciasullo <miaciasullo@gmail.com>
2023-02-28 14:02:18 -0500

    set /api/categories endpoint successfully

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
